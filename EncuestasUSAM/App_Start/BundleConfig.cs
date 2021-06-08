7b71dc16ad3c3d40acd9c8b9d3ea17488c4717c9
Samu0193 <barrerasansesamueldavid@gmail.com>
2021-06-08 11:40:12 -0600

    Configuracion de Bundles, agregue los archivos css y js

---
+++ EncuestasUSAM/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace EncuestasUSAM.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/jquery-2.2.4.min.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/jquery.bootstrap.wizard.js",
+                      "~/Scripts/gsdk-bootstrap-wizard.js",
+                      "~/Scripts/jquery.validate.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/gsdk-bootstrap-wizard.css"));
+        }
+    }
+}
