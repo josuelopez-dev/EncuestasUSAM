9c516a22af41ca399a547dc45655b57a6ccaf294
Jonathan <jonjova1992@hotmail.com>
2021-06-11 21:01:21 -0600

    CRUD Materias

---
+++ EncuestasUSAM/Models/MATERIAScrud.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EncuestasUSAM.Models
+{
+    public class MATERIAScrudInsert
+    {
+        [Required]
+        [Display(Name = "Código Materia")]
+        public string CODIGO_MATERIA { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre Materia")]
+        public string NOMBRE_MATERIA { get; set; }
+
+        [Display(Name = "Carrera")]
+        [ForeignKey(name: "CARRERA")]
+        public int CARRERA { get; set; }
+
+        public int ID_MATERIA { get; set; }
+    }
+
+    public class MATERIAScrudUpdate
+    {
+        [Required]
+        [Display(Name = "Código Materia")]
+        public string CODIGO_MATERIA { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre Materia")]
+        public string NOMBRE_MATERIA { get; set; }
+
+        [Display(Name = "Carrera")]
+        [ForeignKey(name: "CARRERA")]
+        public int CARRERA_ { get; set; }
+
+        public int ID_MATERIA { get; set; }
+    }
+}
